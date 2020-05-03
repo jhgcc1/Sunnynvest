5491f00ddb31654b3dd09c9c7eab2496c0a9aa78
joao <jh_gcc@hotmail.com>
2020-05-02 21:28:20 -0300

    erro test4

--- solinvest/settings.py
+++ solinvest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'REMOVED'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "sunsubversion.herokuapp.com","127.0.0.1","sunnynvest.com","www.sunnynvest.com"
