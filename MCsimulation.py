cf50039e76f024d69ceff820bbff1752348e2aca
joao <jh_gcc@hotmail.com>
2020-06-04 18:12:20 -0300

    comporsell

--- MCsimulation.py
+++ MCsimulation.py
@@ -121,10 +121,6 @@ def simMC(objIpunt):
                 auxList=simMC_result[SimulationYearString]["inputs"][item]["rawData"]
                 auxList.sort()
                 simMC_result[SimulationYearString]["inputs"][item]["rawData"]=auxList
-    if boleanIRR==True:
-        simMC_result["IRRflag"]="True"
-    else:
-        simMC_result["IRRflag"]="False"
     return simMC_result
         
 
